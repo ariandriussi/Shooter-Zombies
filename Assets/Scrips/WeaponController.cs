bd1b77adf52cbd27b24347576d38f32d618d79c6
arian driussi <arian.driussi.dev@gmail.com>
2022-06-22 19:58:53 -0300

    shoot weapon

--- Assets/Scrips/WeaponController.cs
+++ Assets/Scrips/WeaponController.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class WeaponController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("General")]
+    Transform cameraPlayerTransform;
+    public LayerMask hittableLayers;
+    public GameObject bulletHolePrefab;
+
+    [Header("Shoot Parametrs")]
+    public float fireRange = 200;
+
+
+    private void Update()
     {
-        
+        HandlShoot();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Start()
     {
-        
+        cameraPlayerTransform = GameObject.FindGameObjectWithTag("MainCamera").transform;
     }
+    private void HandlShoot()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit;
+         if (Physics.Raycast(cameraPlayerTransform.position, cameraPlayerTransform.forward, out hit, fireRange, hittableLayers))
+            {
+                GameObject bulletHoleCLone = Instantiate(bulletHolePrefab, hit.point + hit.normal * 0.001f, Quaternion.LookRotation(hit.normal));
+                Destroy(bulletHoleCLone, 4f);
+            }
+            
+        }
+    }
+
+
 }
+
+
+   
